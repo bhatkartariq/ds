6218382461d0eea91c0d4737982b75d300fa1f74
Tariq bhatkar <bhatkartariq@eng.rizvi.edu.in>
2026-04-21 20:27:48 +0530

    Update dsa4.c

--- dsa4.c
+++ dsa4.c
@@ -1,77 +1,63 @@
 /*Name: Zain mohamed saeed kazi
 roll no :  40
-uin: 251P037*/
- 
-#include<stdio.h>
-#include<math.h>
-#include<ctype.h>
+uin: 251P037*/ // Author information comment
+#include<stdio.h> // Include standard input output library
+#include<math.h> // Include math library for pow function
+#include<ctype.h> // Include ctype library for isdigit
+int stack[50]; // Declare integer stack array
+int top=-1; // Declare and initialize top to -1
 
-int stack[50];
-int top=-1;
-
-void push(int x)
+void push(int x) // Push function
 {
-     top++;
-     stack[top]=x;
+     top++; // Increment top
+     stack[top]=x; // Assign x to stack
+}
+int pop(){ // Pop function
+    int y; // Declare y
+    y=stack[top]; // Assign top to y
+    top--; // Decrement top
+    return y; // Return y
 }
 
-int pop(){
-    int y;
-    y=stack[top];
-    top--;
-    return y;
-}
 
-
-int main(){
-    char exp[50];
-    int i = 0, op1,op2,result;
-
-    printf("Enter the postfix expression:");
-    scanf("%s",exp);
-
-
-    while (exp[i]!='\0')
+int main(){ // Main function
+    char exp[50]; // Declare expression array
+    int i = 0, op1,op2,result; // Declare variables
+    printf("Enter the postfix expression:"); // Prompt for postfix
+    scanf("%s",exp); // Read expression
+    while (exp[i]!='\0') // Loop through expression
     {
-        if(isdigit(exp[i]))
+        if(isdigit(exp[i])) // If digit
         {
-            push(exp[i]-'0');
-
+            push(exp[i]-'0'); // Push digit value
         }
-        else
+        else // Else operator
         {
-            op1=pop();
-            op2=pop();
-            switch (exp[i])
+            op1=pop(); // Pop op1
+            op2=pop(); // Pop op2
+            switch (exp[i]) // Switch on operator
             {
-            case '+':
-                push(op2+op1);
+            case '+': // Addition
+                push(op2+op1); // Push sum
                 break;
-            
-            case'-':
-                push(op2-op1);
+            case'-': // Subtraction
+                push(op2-op1); // Push difference
                 break;
-            
-            case'*':
-                push(op2*op1);
+            case'*': // Multiplication
+                push(op2*op1); // Push product
                 break;
-            
-            case'/':
-                push(op2/op1);
+            case'/': // Division
+                push(op2/op1); // Push quotient
                 break;
-
-            case'^':
-                push(pow(op2,op1));
+            case'^': // Power
+                push(pow(op2,op1)); // Push power
                 break;
             }
-
         }
-        i++;
+        i++; // Increment i
     }
-    result= pop();
-    printf("\nThe answer of postfix exppression %s= %d\n",exp,result);
-    printf("\ncoder: Zain mohamed saeed kazi");
-    return 0;
-
-
+    result= pop(); // Pop result
+    printf("\nThe answer of postfix exppression %s= %d\n",exp,result); // Print result
+    printf("\ncoder: Zain mohamed saeed kazi"); // Print coder
+    return 0; // Return 0
 }
